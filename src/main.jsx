6f36019587cdf18506da7a3bfc3e58e0b33e603f
bard1229 <99113405+bard1229@users.noreply.github.com>
2025-11-12 19:52:49 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import LineCallback from './LineCallback'
 import './index.css'
+
+// 檢查是否為 LINE Callback 路徑
+const isLineCallback = window.location.pathname === '/auth/line/callback';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isLineCallback ? <LineCallback /> : <App />}
   </React.StrictMode>,
 )
